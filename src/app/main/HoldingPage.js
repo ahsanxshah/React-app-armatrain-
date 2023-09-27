7921ad147b45c0f15f0a9eb24bbdfed9f5dd1f4f
Ahsan Shah <95903222+ahsanxshah@users.noreply.github.com>
2023-09-27 19:56:00 +0500

    Holding Page shown at home.

---
+++ src/app/main/HoldingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HoldingPage.css";
+
+function HoldingPage() {
+  return (
+    <div className="holding-page">
+      <h1 className="coming-soon">Armatrain Coming Soon</h1>
+    </div>
+  );
+}
+
+export default HoldingPage;
